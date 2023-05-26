0cc780a084b134ad11871c43f8a8c59989d7b39c
aluno <aluno>
2023-05-26 15:30:36 -0300

    @DouglasSDQ - Precisa ter uma conta criada e precisa estar com um login valido para entrar

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "counternet",
-    user: "CounterNet",
-    password: "counternet",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
